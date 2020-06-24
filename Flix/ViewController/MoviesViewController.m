b2a3f970d15f7026aa8109d2b3bb9405734a016d
Lauren Tsai <laurentsai@fb.com>
2020-06-24 11:25:09 -0700

    Add refresh control

--- Flix/ViewController/MoviesViewController.m
+++ Flix/ViewController/MoviesViewController.m
@@ -13,6 +13,7 @@
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong) NSArray *movies;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) IBOutlet UIRefreshControl *refreshControl;
 
 @end
 
@@ -24,6 +25,13 @@
     self.tableView.delegate=self;
     
     // Do any additional setup after loading the view.
+    [self fetchMovies];
+    self.refreshControl= [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+- (void) fetchMovies{
+    NSLog(@"Triggered");
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -33,19 +41,20 @@
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-               NSLog(@"%@", dataDictionary);
+               //NSLog(@"%@", dataDictionary);
                // TODO: Get the array of movies
               
                self.movies= dataDictionary[@"results"];
-               for(NSDictionary *movie in self.movies)
+               /*for(NSDictionary *movie in self.movies)
                {
                    NSLog(@"%@", movie[@"title"]);
                    
-               }
+               }*/
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
                [self.tableView reloadData]; //call again bc movies might have changed
            }
+        [self.refreshControl endRefreshing];
        }];
     [task resume];
 }
