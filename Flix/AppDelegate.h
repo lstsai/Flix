851ad78039087142648e95288170ce20f6e2f8f3
Lauren Tsai <laurentsai@fb.com>
2020-06-24 09:42:28 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by laurentsai on 6/24/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by laurentsai on 6/24/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
