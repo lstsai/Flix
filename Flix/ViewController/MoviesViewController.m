abd5860f15fec9f54b3ac924aae2c49813ab4108
Lauren Tsai <laurentsai@fb.com>
2020-06-25 12:03:01 -0700

    add the activity indicator

--- Flix/ViewController/MoviesViewController.m
+++ Flix/ViewController/MoviesViewController.m
@@ -11,10 +11,13 @@
 #import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 
-@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 @property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) NSArray *filteredData;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) IBOutlet UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UISearchBar *seachBar;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -24,18 +27,25 @@
     [super viewDidLoad];
     self.tableView.dataSource=self;
     self.tableView.delegate=self;
-    
+    self.seachBar.delegate=self;
     // Do any additional setup after loading the view.
     [self fetchMovies];//call the method to get the movies from api
+    
     self.refreshControl= [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];//loading refresh and call movie fetch
     [self.tableView insertSubview:self.refreshControl atIndex:0];//move it behind the movies
 }
 - (void) fetchMovies{
+    
+    [self.activityIndicator startAnimating];//***start the activity indicator
+    
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        [self.activityIndicator stopAnimating];//*** stop the indicator
+        
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Networking Error"
@@ -61,6 +71,7 @@
                // TODO: Get the array of movies
               
                self.movies= dataDictionary[@"results"];
+               self.filteredData=self.movies;
                /*for(NSDictionary *movie in self.movies)
                {
                    NSLog(@"%@", movie[@"title"]);
@@ -70,16 +81,17 @@
                // TODO: Reload your table view data
                [self.tableView reloadData]; //call again bc movies might have changed
            }
+        
         [self.refreshControl endRefreshing];//stop refresh load thing
        }];
     [task resume];//actually start the task
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.movies.count;
+    return self.filteredData.count;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     MovieCell *cell= [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];//use the cell that we created
-    NSDictionary *movie= self.movies[indexPath.row];//all the movie titles
+    NSDictionary *movie= self.filteredData[indexPath.row];//all the movie titles
     cell.titleLabel.text=movie[@"title"];//change the title for each movie
     cell.synopLabel.text=movie[@"overview"];
     NSString *baseURL= @"https://image.tmdb.org/t/p/w500";
@@ -92,6 +104,27 @@
 //    cell.textLabel.text= movie[@"title"];//create a cell with movie title
     return cell;
 }
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    
+    if (searchText.length != 0) {
+        
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString *evaluatedObject, NSDictionary *bindings) {
+            return [evaluatedObject containsString:searchText];
+        }];
+        self.filteredData = [self.movies filteredArrayUsingPredicate:predicate];
+        
+        NSLog(@"%@", self.filteredData);
+        
+    }
+    else {
+        self.filteredData = self.movies
+        ;
+    }
+    
+    [self.tableView reloadData];
+ 
+}
+
 
 
 #pragma mark - Navigation
